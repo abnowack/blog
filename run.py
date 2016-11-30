03e5042aae2ffc2c622f456befcde6976985e928
Aaron Nowack <aaronbnowack@gmail.com>
2016-11-30 18:52:12 -0500

    adding files

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import ButteredPost.convert as bpost
 
 if __name__ == '__main__':
-    bpost.build('content', 'docs', info={'name': 'Aaron Nowack'})
+    bpost.build('content', 'docs', info={'name': 'Aaron Nowack', 'root': '/blog'})
     # bpost.serve('example/output')
