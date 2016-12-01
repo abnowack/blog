91bccea506ebf5ce226ff4cb5c5107baf206ed73
Aaron Nowack <aaronbnowack@gmail.com>
2016-11-30 20:26:53 -0500

    Adding publish script

---
+++ publish.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+PATH="/home/anowack/.local/anaconda3/bin:$PATH"
+cd /mnt/c/Users/Aaron\ Nowack/home/GitHub/blog
+
+if python run.py; then
+    echo "Ran ButteredPost - Success";
+else
+    echo "Ran ButteredPost - Failure";
+    read -p "Fix Error and Retry? [y|n]" choice
+    case "$choice" in
+        y|Y ) exec "$0";;
+        n|N ) exit;;
+        * ) exit;;
+    esac
+fi
+
+if [ -n "$(git status --porcelain)" ]; then
+    git add --all
+    git commit -m "adding files"
+else
+    echo "no changes";
+fi
+
+if [ -n "$(git log origin/master..HEAD)" ]; then
+    git push;
+else
+    echo "nothing to push";
+fi
